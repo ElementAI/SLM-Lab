b6e1d6a19f54f9a213f0a252c8ed748c94c127df
Anqi Xu <ax@elementai.com>
2020-07-13 13:49:10 -0400

    Set ROOT_DIR to current working dir (#2)
    * Set ROOT_DIR to current working dir
    # EAI Patch to save to /data/ folder from the current working directory
    
    Co-authored-by: Yanick Chénard <yanick.chenard@elementai.com>

--- slm_lab/__init__.py
+++ slm_lab/__init__.py
@@ -1,7 +1,10 @@
 import os
 
 os.environ['PY_ENV'] = os.environ.get('PY_ENV') or 'development'
-ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+
+# EAI Patch to save to /data/ folder from the current working directory
+ROOT_DIR = os.getcwd()
+# ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
 
 # valid lab_mode in SLM Lab
 EVAL_MODES = ('enjoy', 'eval')
